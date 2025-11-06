a894b32968a4bdf6afd8e6a953f9a99ad0079705
navid-atashbar <atashbar@uci.edu>
2025-11-06 05:30:29 +0000

    added oriignal coce back

--- Checkers_Student/src/checkers-python/StudentAI.py
+++ Checkers_Student/src/checkers-python/StudentAI.py
@@ -14,28 +14,14 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-
     def get_move(self,move):
-        # Just make it caputre the xlosest piecejjj
-        # 
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
-        moves_that_capture_pieces = []
         moves = self.board.get_all_possible_moves(self.color)
-        for elem in moves:
-            for possible_move in elem:
-                if len(possible_move.seq) >1:
-                    moves_that_capture_pieces.append(possible_move)
-
-        if len(moves_that_capture_pieces) > 0:
-            picked_move = moves_that_capture_pieces[randint(0,len(moves_that_capture_pieces) -1)]
-        else:
-            index = randint(0,len(moves)-1)
-            inner_index =  randint(0,len(moves[index])-1)
-            move = moves[index][inner_index]
-            
-      
-        self.board.make_move(picked_move,self.color)
-        return picked_move
+        index = randint(0,len(moves)-1)
+        inner_index =  randint(0,len(moves[index])-1)
+        move = moves[index][inner_index]
+        self.board.make_move(move,self.color)
+        return move
