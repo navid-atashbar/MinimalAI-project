a46631ebce8fe2cc23750d47e906e84d56f90811
TawannAlvarez <tawann@lazymonkey.net>
2025-11-07 00:27:42 +0000

    attempting to integrate slide code into the game. Not fullt done

--- Checkers_Student/src/checkers-python/StudentAI.py
+++ Checkers_Student/src/checkers-python/StudentAI.py
@@ -20,35 +20,50 @@ class StudentAI():
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
+        move = minimax_search(self.board, self.color)
+        '''index = randint(0,len(moves)-1)
         inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
+        move = moves[index][inner_index]'''
         self.board.make_move(move,self.color)
         return move
-        '''
-        def minimax_search(game, state):
-            player = game.to_move(state)
-            value, move = max_value(game,state)
-            return move
-        def max_value(game,state):
-            if game.is_terminal(state):
-                return game.utility(state, game.to_move(state)), None
-            v = float('-inf')
-            best_move = None
-            for a in game.actions(state):
-                v2, _ = min_value(game, game.result(state,a))
-                if v2 > v:
-                    v, best_move = v2, a
-            return v, best_move
-        def min_value(game, state):
-            if game.is_terminal(state):
-                return game.utility(state,game.to_move(state)), None
-            
-            v = float('inf')
-            best_move = None
-            for a in game.actions(state):
-                v2, _ = max_value(game,game.result(state,a))
-                if v2 < v:
-                    v, best_move = v2, a
-            return v, best_move
-                '''
+
+    def minimax_search(game, state):
+        player = game.to_move(state)
+        value, move = max_value(game,state)
+        return move
+        
+    def max_value(game,state):
+        player = game.to_move(state)
+        result = game.is_win(state)
+        if result != 0:
+            if result == -1:
+                return 0, None
+            elif result == player:
+                return 100, None
+            else:
+                return -100, None
+        v = float('-inf')
+        best_move = None
+        for a in game.get_all_possible_moves(state):
+            v2, _ = min_value(game, game.result(state,a))
+            if v2 > v:
+                v, best_move = v2, a
+        return v, best_move
+
+    def min_value(game, state):
+        player = game.to_move(state)
+        result = game.is_win(state)
+        if result != 0:
+            if result == -1:
+                return 0, None
+            elif result == player:
+                return 100, None
+            else:
+                return -100, None
+        v = float('inf')
+        best_move = None
+        for a in game.get_all_possible_moves(state):
+            v2, _ = max_value(game,game.result(state,a))
+            if v2 < v:
+                v, best_move = v2, a
+        return v, best_move
