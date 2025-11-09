561c4d41aee4d8a4791d19f3fe3644f8e676536e
TawannAlvarez <tawann@lazymonkey.net>
2025-11-09 06:11:26 +0000

    did alpha beta pruing to increase effiecency

--- Checkers_Student/src/checkers-python/StudentAI.py
+++ Checkers_Student/src/checkers-python/StudentAI.py
@@ -14,7 +14,9 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-        self.max_depth_search = 5
+        self.max_depth_search = 8
+        self.alpha = float('-inf')
+        self.beta = float('inf')
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
@@ -23,7 +25,7 @@ class StudentAI():
         moves = self.board.get_all_possible_moves(self.color)
         if not moves or all(len(m) == 0 for m in moves):
             return Move([])
-        move = self.minimax_search(self.board, self.color, self.max_depth_search)
+        move = self.minimax_search(self.board, self.color, self.max_depth_search, self.alpha, self.beta)
         '''index = randint(0,len(moves)-1)
         inner_index =  randint(0,len(moves[index])-1)
         move = moves[index][inner_index]'''
@@ -37,12 +39,12 @@ class StudentAI():
             self.board.make_move(move, self.color)
             return move 
 
-    def minimax_search(self, game, state, depth):
+    def minimax_search(self, game, state, depth, alpha, beta):
         #player = game.to_move(state)
-        value, move = self.max_value(game,state,depth)
+        value, move = self.max_value(game,state,depth, alpha, beta)
         return move
         
-    def max_value(self, game,state, depth):
+    def max_value(self, game,state, depth, alpha, beta):
         #Tawann add the if statement to check if we went over the depth here
         if depth == 0:
             return 0, None
@@ -67,15 +69,19 @@ class StudentAI():
             for j in i:
                 try:
                     game.make_move(j,state)
-                    v2, _ = self.min_value(game, opponent, depth -1)
+                    v2, _ = self.min_value(game, opponent, depth -1, alpha, beta)
                     game.undo()
                     if v2 > v:
-                        v, best_move = v2, j
+                        v = v2
+                        best_move = j
+                    if v >= beta:
+                        return v, best_move
+                    alpha = max(alpha, v)
                 except:
                     continue
         return v, best_move
 
-    def min_value(self, game, state, depth):
+    def min_value(self, game, state, depth, alpha, beta):
         #Tawann add the if statement to check if we went over the depth here
         #If we did then return 0,None
         if depth == 0:
@@ -98,10 +104,19 @@ class StudentAI():
             for j in i:
                 try:
                     game.make_move(j,state)
-                    v2, _ = self.max_value(game, opponent, depth -1)
+                    v2, _ = self.max_value(game, opponent, depth -1, alpha, beta)
                     game.undo()
                     if v2 < v:
-                        v, best_move = v2, j
+                        v = v2
+                        best_move = j
+                    if v <= alpha:
+                        return v, best_move
+                    beta = min(beta, v)
                 except:
                     continue
         return v, best_move
+    def get_score(self, game, state):
+        score = 0
+        
+
+        return 
