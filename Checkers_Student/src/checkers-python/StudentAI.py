2bb44974aea8e16b6cce52a23267eb6ed362c998
navid-atashbar <atashbar@uci.edu>
2025-11-20 05:10:06 +0000

    Added comments for the plan to implement MCTS

--- Checkers_Student/src/checkers-python/StudentAI.py
+++ Checkers_Student/src/checkers-python/StudentAI.py
@@ -14,162 +14,57 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-        self.max_depth_search = 5
-        self.alpha = float('-inf')
-        self.beta = float('inf')
+
     def get_move(self,move):
-        #IMPLEMENT MONTE CARLO TREE SEARCH
+        #1 handle opps move if they made one
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
+        #2 get all legal moves for curr player
+        #if none return empty move
+        #call mcts
+        #take the best move on board and return it
         moves = self.board.get_all_possible_moves(self.color)
-        legal_moves = []
-        for group in moves:
-            for m in group:
-                if m is not None:
-                    legal_moves.append(m)
-        if not moves or all(len(m) == 0 for m in moves):
-            return Move([])
-        move = self.minimax_search(self.board, self.color, self.max_depth_search, self.alpha, self.beta)
-        '''index = randint(0,len(moves)-1)
+        index = randint(0,len(moves)-1)
         inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]'''
-        if move is not None and move in legal_moves:
-            self.board.make_move(move,self.color)
-            return move
-        else:
-            print("random move")
-            index = randint(0, len(moves)-1)
-            inner_index = randint(0, len(moves[index])-1)
-            move = moves[index][inner_index]
-            self.board.make_move(move, self.color)
-            return move 
-
-    def minimax_search(self, game, state, depth, alpha, beta):
-        #used alpha beta and min max from slides
-        #player = game.to_move(state)
-        value, move = self.max_value(game,state, depth, alpha, beta)
+        move = moves[index][inner_index]
+        self.board.make_move(move,self.color)
         return move
-        
-    def max_value(self, game,state, depth, alpha, beta):
-        #Tawann add the if statement to check if we went over the depth here
-        if depth <= 0:
-            return self.get_score(game, self.color), None
-        #If we did then return 0,None
-        result = game.is_win(state)
-        if result != 0:
-            if result == state:
-                return 1000, None
-            elif result == self.opponent[self.color]:
-                return -1000, None
-            else:
-                return 0, None
-        all_moves = game.get_all_possible_moves(state)
-        if not all_moves or self.len_checker(all_moves):
-            return self.get_score(game, state), None
 
-        v = float('-inf')
-        best_move = None
-        opponent = self.opponent[state]
-        all_moves = game.get_all_possible_moves(state)
-        for i in all_moves:
-            for j in i:
-                try:
-                    try:
-                        game.make_move(j,state)
-                    except:
-                        continue
-                    v2, _ = self.min_value(game, opponent, depth -1, alpha, beta)
-                    game.undo()
-                    if v2 > v:
-                        v = v2
-                        best_move = j
-                    if v >= beta:
-                        return v, best_move
-                    alpha = max(alpha, v)
-                except:
-                    continue
-        return v, best_move
 
-    def min_value(self, game, state, depth, alpha, beta):
-        #Tawann add the if statement to check if we went over the depth here
-        #If we did then return 0,None
-        if depth <= 0:
-            return self.get_score(game, state), None
-        result = game.is_win(state)
-        if result != 0:
-            if result == self.opponent[self.color]:
-                return 1000, None
-            elif  result == -1:
-                return -1000, None
-            else:
-                return 0, None
-        all_moves = game.get_all_possible_moves(state)
-        if not all_moves or self.len_checker(all_moves):
-            return self.get_score(game, state), None
-        v = float('inf')
-        best_move = None        
-        opponent = self.opponent[state]
-        for i in all_moves:
-            for j in i:
-                try:
-                    try:
-                        game.make_move(j,state)
-                    except:
-                        continue
-                    v2, _ = self.max_value(game, opponent, depth -1, alpha, beta)
-                    game.undo()
-                    if v2 < v:
-                        v = v2
-                        best_move = j
-                    if v <= alpha:
-                        return v, best_move
-                    beta = min(beta, v)
-                except:
-                    continue
-        return v, best_move
-    def get_score(self, game, state):
-        # calculates score for huiristic
-        score = 0
-        board = game.board
-        color_map = {1: "B", 2: "W"} 
-        my_color = color_map[self.color]
-        opp_color = color_map[self.opponent[self.color]]
-        
-        my_pieces = 0
-        opp_pieces = 0
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                piece = board[i][j]
-                if piece is None:
-                    continue
-                if piece.color == my_color:
-                    score += 1
-                    if piece.is_king:
-                        score += 7
-                    else:
-                        score +=3
-                        if my_color =="B":
-                            score+= (self.row-1-i) *0.5
-                        else:
-                            score += i *0.5
-                    
-                    
-                elif piece.color == opp_color:
-                    score -= 1
-                    if piece.is_king:
-                        score -= 7
-                    else:
-                        score +=3
-                        if my_color =="B":
-                            score-= (self.row-1-i) *0.5
-                        else:
-                            score -= i *0.5
-        score += (my_pieces - opp_pieces) *2
-        return score
-    def len_checker(self, iterablee):
-        for x in iterablee:
-            if len(x) != 0:
-                return False
-        return True
+
+    def monty_search(self,legal_moves):
+        #dictinoary to store stats for each of the moves wins and simulatiobs
+        # all moves start with 0 wins and 0 simulation
+        #loop number of simulations
+            #select move using uct
+            #simulate randome game from the move
+            #get result
+            #update stats
+
+        #pick the move with the highest win rate
+        #return that move
+
+    def pick_move(self,move_dictionary, legal_moves):
+        #calc total sims across all moves
+        #check each move if any have 0 sims return it to explore
+        #for each move get the UCT
+            #get wins and somulations for this move
+            #calc the wins/simulation
+            #calc explorations  
+            #UCT = wins/sims + explorations
+        #return the move with the highest UCT value
+
+    def game_simulator(self,start_move):
+        #make a copy of the curr board
+        #make the start move
+        #set curr player to opponent
+        #loop until games end
+            #check if games won return winner 
+            #get legal moves for curr player
+            #if no moves return opp as the winner
+            # pick a random move from legal moves
+            #make the random move
+            #switch to other player
+        #if max moves reached evalute board and return likely winner
